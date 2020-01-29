f7900570ec36ff59d7d8cafc9e87a9e390d7621b
danWagar <dandwagar@gmail.com>
2020-01-29 12:41:14 -0800

    recommended changes made

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,11 @@ export default function HomePage(props) {
   return (
     <>
       <img src={homeImage} alt="cute puppy"></img>
-      <p>testing</p>
+      <p>
+        At the Petful adoption center we only put up one pet for adoption at a time, namely whichever pet has
+        been at the adoption center the longest amount of time. If you would like to get in line to adopt a
+        pet just press the 'Adopt Now' button below.
+      </p>
       <Link to="/adopt">
         <button>Adopt Now</button>
       </Link>
